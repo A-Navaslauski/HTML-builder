244c7b5c0d0094b22908dc8d426b3511b1190637
Anton Navaslauski <anton_ivanov_2000@list.ru>
2022-05-22 15:34:55 +0200

    feat: complete 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+let filesCopy;
+let files;
 
+(async () => {
+        await fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+		filesCopy = await fs.promises.readdir(path.join(__dirname, 'files-copy'))
+		files = await fs.promises.readdir(path.join(__dirname, 'files'))
+		if (filesCopy.length > 0) {
+			for (let file of filesCopy) {
+				fs.promises.unlink(path.join(__dirname, 'files-copy', file))
+			}
+		}
+
+		if (files.length > 0) {
+			for (let file of files) {
+				fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file))
+			}
+		}
+})()
